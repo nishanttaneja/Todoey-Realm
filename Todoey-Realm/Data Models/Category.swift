ec64d70781cd8fd83f42e7a0d6d7eccdb47cf6a2
nishanttaneja <nishant.taneja2701@gmail.com>
2020-09-10 19:24:23 +0530

    add DataModels

---
+++ Todoey-Realm/Data Models/Category.swift
@@ -0,0 +1,16 @@
+//
+//  Category.swift
+//  Todoey-Realm
+//
+//  Created by Nishant Taneja on 10/09/20.
+//  Copyright © 2020 Nishant Taneja. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Category: Object {
+    @objc dynamic var name: String = ""
+    @objc dynamic var colour: String = ""
+    let items = List<Item>()
+}
