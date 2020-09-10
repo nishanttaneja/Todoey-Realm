a0a6899a0e27af2bac50b3d3eb7b9c5f781a38c3
nishanttaneja <nishant.taneja2701@gmail.com>
2020-09-10 19:35:02 +0530

    implement TableView DataSource|Delegate

--- Todoey-Realm/Controllers/CategoryTableViewController.swift
+++ Todoey-Realm/Controllers/CategoryTableViewController.swift
@@ -11,21 +11,38 @@ import RealmSwift
 
 class CategoryTableViewController: UITableViewController {
     //MARK:- Constant|Variable
-    let realm = try! Realm()
+    private let realm = try! Realm()
+    private var categories: Results<Category>?
+    
+    //MARK:- Override ViewLifecycle Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadCategories()
+    }
     
     //MARK:- IBAction
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {}
     
     //MARK:- TableView DataSource|Delegate
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return categories?.count ?? 1
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        cell.textLabel?.text = categories?[indexPath.row].name ?? "No categories added yet."
         return cell
     }
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {}
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "CategoryToItemScene", sender: self)
+    }
     
     //MARK:- Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {}
+    
+    //MARK:- Realm CRUD
+    /// This function is executed to load data.
+    private func loadCategories() {
+        categories = realm.objects(Category.self)
+        tableView.reloadData()
+    }
 }
